5da6c98fcde7395820655faf81933e65428cdef1
DavidBednarik <davidbednarik07@gmail.com>
2023-05-27 13:21:02 +0200

    basic layout with first data fetch

---
+++ src/components/layout/to-do-layout.tsx
@@ -0,0 +1,25 @@
+import { Category } from "@/models/category";
+import { getCategories } from "../../../api";
+import TodoCategory from "../categories/todo-category";
+import styles from "./todo-layout.module.css";
+
+const ToDoLayout = async () => {
+	const categories: Category[] = await getCategories();
+
+	const categoriesData = categories.map((category: Category) => (
+		// eslint-disable-next-line react/jsx-key
+		<div>
+			{/* @ts-expect-error Async Server Component, reason: https://nextjs.org/docs/app/building-your-application/data-fetching/fetching */}
+			<TodoCategory category={category} key={category.id} />
+		</div>
+	));
+
+	return (
+		<div className={styles.container}>
+			<h3 className={styles.title}>To do list</h3>
+			<div className={styles.categoryContainer}>{categoriesData}</div>
+		</div>
+	);
+};
+
+export default ToDoLayout;
