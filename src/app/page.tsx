21e43a32219ddf48e60289fa45c741e655f68c9c
DavidBednarik <davidbednarik07@gmail.com>
2023-05-29 09:24:47 +0200

    removed unused comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import ToDoLayout from "@/components/layout/to-do-layout";
 export default function Home() {
 	return (
 		<main>
-			{/* @ts-expect-error Async Server Component, reason: https://nextjs.org/docs/app/building-your-application/data-fetching/fetching */}
 			<ToDoLayout />
 		</main>
 	);
