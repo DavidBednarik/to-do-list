f3245317a5953e8c56cca3118f643856b158ee33
DavidBednarik <davidbednarik07@gmail.com>
2023-05-29 15:10:36 +0200

    react query implementation

--- src/components/layout/to-do-layout.tsx
+++ src/components/layout/to-do-layout.tsx
@@ -4,10 +4,23 @@ import TodoCategory from "../categories/todo-category";
 import styles from "./todo-layout.module.css";
 import Filter from "../filter/filter";
 import AddCategory from "../categories/add-category";
-import { useGetCategories } from "../categories/use-get-categories";
+import { getCategoriesAPI } from "@/endpoints/endpoints";
+import { useQuery } from "react-query";
 
 const ToDoLayout = () => {
-	const { data: categories } = useGetCategories();
+	const getCategories = async () => {
+		const res = await fetch(getCategoriesAPI);
+		return res.json();
+	};
+
+	const {
+		data: categories,
+		error,
+		isLoading,
+	} = useQuery({
+		queryKey: "ToDoCategories",
+		queryFn: getCategories,
+	});
 
 	const categoriesData = categories?.map((category: Category) => (
 		<TodoCategory category={category} key={category.id} />
